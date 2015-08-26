aca6d29e87a2c97047445a515d83283e58af11a7
nboehning <nboehning@gmail.com>
2015-08-26 09:53:11 -0500

    Added and Completed create prefab tool, started 2d animation tool
    Completed the create prefab tool. Started the create 2D animation tool

---
+++ Lab02BoehningNathanProject/Assets/Editor/Script2DSpriteAutomation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+public class Script2DSpriteAutomation : EditorWindow 
+{
+	public static Object selectedObject;
+
+	int numAnimations;
+	string controllerName;
+	string[] animationNames = new string[100];
+	float[] clipFrameRate = new float[100];
+	float[] cliopTimeBetween = new float[100];
+	int[] startFrames = new int[100];
+	int[] endFrames = new int[100];
+	bool[] pingPong = new bool[100];
+	bool[] loop = new bool[100];
+
+
+	[MenuItem("Project Tools/2D Animations")]
+	public static void Init()
+	{
+		selectedObject = Selection.activeObject;
+
+		if(selectedObject == null)
+		{
+			return;
+		}
+
+		Script2DSpriteAutomation window = (Script2DSpriteAutomation)EditorWindow.GetWindow (typeof(Script2DSpriteAutomation));
+		window.Show();
+	}
+}
