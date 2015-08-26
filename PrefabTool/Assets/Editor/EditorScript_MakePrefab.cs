e666c0bb969501928c572fa2647f5f4521995804
agentronin316 <svc-student@RC37816.student.directory.jccc>
2015-08-26 09:49:10 -0500

    Prefab Creation Implemented
    And Working!! Muahahaha!

---
+++ PrefabTool/Assets/Editor/EditorScript_MakePrefab.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class EditorScript_MakePrefab : MonoBehaviour {
+	[MenuItem("Project Tools/Create Prefab")]
+	public static void MakePrefab()
+	{
+		GameObject[] selectedObjects = Selection.gameObjects;
+
+		foreach(GameObject go in selectedObjects)
+		{
+			string name = go.name;
+			string assetPath = "Assets/" + name + ".prefab";
+
+			if(AssetDatabase.LoadAssetAtPath (assetPath, typeof(GameObject)))
+			{
+				//Debug.Log ("Asset existed");
+				if(EditorUtility.DisplayDialog ("Prefab already exists", "Prefab already exists. Do you want to overwrite?", "Overwrite", "Cancel"))
+				{
+					CreateNew (go, assetPath);
+				}
+			}
+			else
+			{
+				CreateNew (go, assetPath);
+			}
+			//Debug.Log ("Name: " + go.name + " Path: " + assetPath);
+
+		}
+	}
+
+
+	public static void CreateNew(GameObject obj, string location)
+	{
+		Object prefab = PrefabUtility.CreateEmptyPrefab (location);
+		PrefabUtility.ReplacePrefab (obj, prefab);
+		DestroyImmediate (obj);
+		GameObject clone = PrefabUtility.InstantiatePrefab (prefab) as GameObject;
+	}
+}
